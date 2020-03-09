2ab9f0bc81d9cafdc5f35dbaee3ef702e80c78a9
Steven Leadbeater <info@ledsysconsultancy.com>
2020-03-09 08:12:07 +0000

    tests

---
+++ src/binary_search.rs
@@ -0,0 +1,57 @@
+struct BinarySearch {
+    sorted_array: Vec<i64>,
+}
+
+impl BinarySearch {
+    fn new(array: Vec<i64>) -> BinarySearch {
+        BinarySearch {
+            sorted_array: array,
+        }
+    }
+
+    fn search(&self, target: i64) -> i64 {
+        return target;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::binary_search::BinarySearch;
+
+    #[test]
+    fn left() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(33), 4);
+    }
+
+    #[test]
+    fn right() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(84), 10);
+    }
+
+    #[test]
+    fn not_found_left() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(34), -1);
+    }
+
+    #[test]
+    fn not_found_right() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(85), -1);
+    }
+
+    #[test]
+    fn far_left() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(6), 0);
+    }
+
+    #[test]
+    fn far_right() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(97), 15);
+    }
+
+}
