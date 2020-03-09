fdbbb7e5f629611bedde8ed78a3069c34f53cb74
Steven Leadbeater <info@ledsysconsultancy.com>
2020-03-09 08:36:57 +0000

    implementation

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -10,13 +10,35 @@ impl BinarySearch {
     }
 
     fn search(&self, target: i64) -> i64 {
-        return target;
+        let mut local_array = self.sorted_array.clone();
+        let mut basis = 0;
+        loop {
+            let index = (local_array.len() / 2);
+            if local_array[index] == target {
+                return (basis + index) as i64;
+            } else if local_array[index] > target {
+                local_array = local_array.split_at(index).0.to_vec();
+            } else if local_array[index] < target {
+                local_array = local_array.split_at(index).1.to_vec();
+                basis += index;
+            }
+            if local_array.len() == 1 && local_array[0] != target {
+                break;
+            }
+        }
+        -1
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::binary_search::BinarySearch;
+
+    #[test]
+    fn center() {
+        let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
+        assert_eq!(searcher.search(53), 7);
+    }
 
     #[test]
     fn left() {
@@ -51,7 +73,7 @@ mod tests {
     #[test]
     fn far_right() {
         let searcher = BinarySearch::new(vec![6, 13, 14, 25, 33, 43, 51, 53, 64, 72, 84, 93, 95, 96, 97]);
-        assert_eq!(searcher.search(97), 15);
+        assert_eq!(searcher.search(97), 14);
     }
 
 }
